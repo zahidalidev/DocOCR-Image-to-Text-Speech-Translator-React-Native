d4936f6dc2f81cf6b7be1e89e782b4248507eecc
zahidalidev <m.zahidalidev@gmail.com>
2021-01-07 04:37:00 +0500

    saving image in nodejs and convert to text

--- backendNode/routes/ocr.js
+++ backendNode/routes/ocr.js
@@ -1,36 +1,38 @@
 const express = require('express')
 const tessaract = require('tesseract.js')
 const multer = require('multer');
+const fs = require('fs-extra')
 
-const app = express()
+const router = new express.Router()
 
-app.use(express.static('public'))
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './')
+        cb(null, './assets/images/')
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + '-' + file.originalname)
     }
 });
+
 const upload = multer({ storage })
 
-const router = new express.Router()
-
 router.post('/', upload.single('file'), async (req, res) => {
-    console.log(req.file)
-    // const createWorker = tessaract.createWorker;
-    // const worker = createWorker({
-    //     logger: m => console.log(m),
-    // });
+    try {
+        const createWorker = tessaract.createWorker;
+        const worker = createWorker();
+        await worker.load();
+        await worker.loadLanguage('eng');
+        await worker.initialize('eng')
 
-    // await worker.load();
-    // await worker.loadLanguage('eng');
-    // await worker.initialize('eng')
-    // const { data: { text } } = await worker.recognize(req.file)
+        const { data: { text } } = await worker.recognize(req.file.path)
 
-    // console.log(text)
-    res.send('text')
+        // remove image from path "req.file.path"
+        await fs.remove(req.file.path)
+
+        res.send(text)
+    } catch (error) {
+        res.send(error)
+    }
 })
 
 
